305c90aea5390392c1836d20e7cc5faaedd4b116
luisfsborsari <luisfsborsari@gmail.com>
2017-11-20 15:26:47 -0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-//No Heroku a porta está em process.env.PORT. Na máquina local é 3000
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -25,31 +24,51 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-      res.send({todos});
-    }, (err) => {
-      res.status(400).send(err);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todos/:id', (req, res) =>{
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo){
-      res.status(404).send();
-    }else {
-      res.status(200).send({todo});
+    if (!todo) {
+      return res.status(404).send();
     }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
